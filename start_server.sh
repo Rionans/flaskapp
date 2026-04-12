4c78b3942cada2ad6d5c96b6abd845186f95bb39
Don <udushka@gmail.com>
2026-04-12 08:22:05 +0000

    changed start_server.sh, created wsgi.py, changed appveyor.yml

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-apt-get update && apt-get install python3-pip python3-dev python3-venv
-python3 -m venv venv
-source venv/bin/activate
-pip install flask gunicorn
+sudo apt-get update && sudo apt-get install python3-pip python3-dev
+sudo python3 -m pip install -U flask gunicorn
