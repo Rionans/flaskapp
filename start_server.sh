fb3701634755506fea7413721ea81ec30b9477eb
Don <udushka@gmail.com>
2026-04-12 09:05:43 +0000

    modified start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install python3-pip python3-dev
-sudo python3 -m pip install -U flask gunicorn
+sudo apt-get update && sudo apt-get install python3-pip python3-dev python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip install -U flask gunicorn requests
